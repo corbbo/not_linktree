693eefa17ed4c615a5af7994f01ba21c2e9ec356
corbbo <thiagozilber@gmail.com>
2023-12-26 18:43:11 -0300

    more work

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
     import AuthCheck from "$lib/components/AuthCheck.svelte";
-    import { databank, user } from "$lib/firebase";
+    import { databank, user, userData } from "$lib/firebase";
     import { doc, getDoc, writeBatch } from "firebase/firestore";
+
 
     let username = "";
     let loading = false;
     let isAvailable = false;
 
     let debounceTimer: NodeJS.Timeout;
+
+    const re = /^(?=[a-zA-Z0-9._]{3,16}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+    $: isValid = username?.length > 2 && username.length < 16 && re.test(username);
+    $: isTouched = username?.length > 0;
+    $: isTaken = isValid && !isAvailable && !loading;
 
     async function checkAvailability() {
         loading = true;
@@ -24,13 +31,36 @@
     }
 
     async function confirmUsername() {
+        console.log("confirming username...");
+        const batch = writeBatch(databank);
+        batch.set(doc(databank, "usernames", username), {uid: $user?.uid});
+        batch.set(doc(databank, "users", $user!.uid), {
+            username,
+            photoURL: $user?.photoURL ?? null,
+            published: true,
+            bio: 'I am the eggman',
+            links: [
+                {
+                    title: 'test',
+                    url: 'http://google.com',
+                    icon: 'custom'
+                }
+            ]
+        });
+
+        await batch.commit();
+
+        username = '';
+        isAvailable = false;
     }
 
 
 </script>
 
 <AuthCheck>
-    <h2>Username</h2>
+    {#if $userData?.username}
+        <p class="text-center text-2xl">You already have a username: <a href="/@{$userData.username}">@{$userData.username}</a></p>
+    {/if}
     <form class="w-2/5" on:submit|preventDefault={confirmUsername}>
         <input
             type="text"
@@ -38,11 +68,22 @@
             class="input w-full"
             bind:value={username}
             on:input={checkAvailability}
+            class:input-error={(!isValid && isTouched)}
+            class:input-warning={(isTaken)}
+            class:input-success={(isAvailable && isValid && !loading)}
         />
-
-        <p>Is available? {isAvailable}</p>
-
-        <button class="btn btn-success">Confirm username @{username}</button>
-    </form>
-
+        <div class="my-4 min-h16 px-8 w-full">
+            {#if loading}
+                <p class="text-secondary">Checking availability of @{username}...</p>
+            {/if}
+            {#if !isValid && isTouched}
+                <p class="text-error text-sm">must be 3-16 characters long, alphanumeric only</p>
+            {/if}
+            {#if isValid && !isAvailable && !loading}
+                <p class="text-warning text-sm">@{username} is already taken</p>
+            {/if}
+            {#if isValid && isAvailable && !loading}
+                <button class="btn btn-success">Confirm username @{username}</button>
+            {/if}
+        </div>
 </AuthCheck>
